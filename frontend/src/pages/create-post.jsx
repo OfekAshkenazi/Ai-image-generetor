f98358539db076a4225228be52b596a3e00f716c
ofek ashkenazi <ofeka18@gmail.com>
2023-04-16 18:35:43 +0300

    worked on the form for generate image.

--- frontend/src/pages/create-post.jsx
+++ frontend/src/pages/create-post.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom'
 import { getRandomPrompt } from '../utils'
 import { FormField, Loader } from '../components'
 
-
 export default function CreatePost() {
     const navigate = useNavigate()
     const [generatingImg, setGeneratingImg] = useState(false)
@@ -21,33 +20,77 @@ export default function CreatePost() {
     }
 
     function handleChange(event) {
-
+        setForm({ ...form, [event.target.name]: event.target.value })
     }
 
     function handlesupriseMe() {
-        
+        const randomPrompt = getRandomPrompt(form.prompt)
+        setForm({ ...form, prompt: randomPrompt })
+    }
+
+    function generateImage() {
+
     }
 
     return (
         <section className="max-w-7xl mx-auto">
 
             <div className="">
+
                 <h1 className="font-extrabold -text-[#222328] text-[32px]">Create</h1>
+                
                 <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Create imaginative and Visually stunning images through by DALL-E AI and share them with the community</p>
+           
             </div>
 
-
             <form className="mt-16 max-w-3xl" onSubmit={handleSumbit}>
-               
-               
+
                 <div className="flex flex-col gap-5">
-                    <FormField labelName="Your name" type="text" name="name" placeholder="..." value={form.name} handleChange={handleChange}/>
-                    <FormField labelName="Prompt" type="text" name="Prompt" placeholder="a painting of a fox in the style of Starry Night" value={form.prompt} handleChange={handleChange} isSupriseMe handlesupriseMe={handlesupriseMe}/>
+
+                    <FormField labelName="Your name" type="text" name="name" placeholder="..." value={form.name} handleChange={handleChange} />
+                    <FormField labelName="Prompt" type="text" name="Prompt" placeholder="a painting of a fox in the style of Starry Night" value={form.prompt} handleChange={handleChange} isSupriseMe handlesupriseMe={handlesupriseMe} />
+
+                    <div className=" relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+
+                        {form.photo ? (
+                            <img src={form.photo} alt={form.prompt} className="w-full h-full object-contain" />
+                        ) : (
+                            <img src={preview} alt="preview" className="w-9/12 h-9/12 object-contain opacity-40" />
+                        )}
+
+
+                        {generatingImg && (
+
+                            <div className=" absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                                <Loader />
+                            </div>
+                        )}
+
+                    </div>
 
                 </div>
 
+                <div className="mt-5 flex gap-5 ">
+
+                    <button type="button" className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center" onClick={generateImage}>
+                        {generatingImg ? 'Generating...' : 'Generate'}
+
+                    </button>
+
+                </div>
+
+                <div className="mt-10">
+
+                    <p className="mt-2 text-[#666e75] text-[14px]">Once you have created the image if you want, you can share it with others.</p>
+                   
+                    <button type="submit" className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                        {loading ? 'Sharing...' : 'Share'}
+                    </button>
+
+                </div>
 
             </form>
+            
         </section>
     )
 }

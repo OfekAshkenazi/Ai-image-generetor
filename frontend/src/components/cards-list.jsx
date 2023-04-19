4c47ec072c7367a200cd1932802470001571687d
ofek ashkenazi <ofeka18@gmail.com>
2023-04-19 11:53:23 +0300

    added hover effect to card that show the prompt and the name of the one who shere it. added the option to download the photo with filesaver.

--- frontend/src/components/cards-list.jsx
+++ frontend/src/components/cards-list.jsx
@@ -1,8 +1,8 @@
 import Card from "./card"
 
 export default function CardsList({ data, title }) {
-    
-    if (data?.length > 0) return data.map((post) => <Card key={post.id} {...post} />)
+
+    if (data?.length > 0) return data.map((post) => <Card key={post._id} {...post} />)
     return (
         <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
             {title}

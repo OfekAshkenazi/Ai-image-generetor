e70501508a84486725fc4cc5ca7c816b44bf9b96
ofek ashkenazi <ofeka18@gmail.com>
2023-04-19 13:22:22 +0300

    trying to deploey

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 
-
 app.use('/api/v1/post', postRoutes)
 app.use('/api/v1/dalle', dalleRoutes)
 
@@ -28,7 +27,6 @@ async function runServer() {
 runServer()
 
 
-/// for test the backend work
-// app.get('/', async (req, res) => {
-//     res.send('hello ofek your server is working')
-// })
+app.get('/', async (req, res) => {
+    res.send('hello ofek your server is working')
+})
